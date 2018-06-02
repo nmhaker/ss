73e524b863ed924356feb9b8a9d5a0dd0c8227a6
Nikola Milutinovic <nmhaker@gmail.com>
2018-06-03 00:57:58 +0200

    Uspesno ucitan fajl preko liste
    Stringovi potom ubaceni u array zbog indeksiranja jedan na jedan

---
+++ include/array.h
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   array.h
+ * Author: buki
+ *
+ * Created on June 3, 2018, 12:24 AM
+ */
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#include <list>
+#include <string>
+
+class Array{
+
+public:
+	Array(std::list<std::string>*);
+	~Array();
+
+	void printArray();
+private:
+	std::string** array;
+	int size;
+};
+
+
+#endif /* ARRAY_H */
+
